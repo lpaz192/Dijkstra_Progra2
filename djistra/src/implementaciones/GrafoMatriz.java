a953e8ebdeefc2fecfbd49197dbd5e7787a08046
LGhiglionee <lghiglione@uade.edu.ar>
2025-05-25 21:38:39 +0000

    PRUEBA

--- djistra/src/implementaciones/GrafoMatriz.java
+++ djistra/src/implementaciones/GrafoMatriz.java
@@ -1,6 +1,9 @@
 import implemetaciones.GrafoTDA;
 
 
-public class GrajoMatriz implements GrafoTDA {
-
+public class GrafoMatriz implements GrafoTDA {
+    static int n = 100;
+    int [][] MAdy;
+    int [] Etiqs;
+    int cantNodos;
 }
